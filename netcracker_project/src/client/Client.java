05ae511e53711a575ed38f70dfb8477d2b2f4d9c
antonyvol <antonyvol1@gmail.com>
2016-04-09 23:29:11 +0300

    add form for adding cars

--- netcracker_project/src/client/Client.java
+++ netcracker_project/src/client/Client.java
@@ -1,6 +1,9 @@
 package client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 
 /**
@@ -8,7 +11,62 @@ import com.google.gwt.user.client.ui.*;
  */
 public class Client implements EntryPoint {
     public void onModuleLoad() {
-        Label label = new Label("Hello Gleb!");
-        RootPanel.get().add(label);
+        final String TEXTBOX_LENGTH = "220";
+
+        // форма добавления автомобиля в базу
+        final FormPanel carAddForm = new FormPanel();
+
+        // панель, содержащая поля формы
+        final VerticalPanel carAddPanel = new VerticalPanel();
+        carAddPanel.setSpacing(10);
+
+        final TextBox carModel = new TextBox();
+        carModel.setWidth(TEXTBOX_LENGTH);
+        carModel.setName("carModelTextBox");
+        carAddPanel.add(carModel);
+
+        final TextBox carRegplate = new TextBox();
+        carRegplate.setWidth(TEXTBOX_LENGTH);
+        carRegplate.setName("carRegplateTextBox");
+        carAddPanel.add(carRegplate);
+
+        final TextBox carYear = new TextBox();
+        carYear.setWidth(TEXTBOX_LENGTH);
+        carYear.setName("carYearTextBox");
+        carAddPanel.add(carYear);
+
+        final TextBox carPrice = new TextBox();
+        carPrice.setWidth(TEXTBOX_LENGTH);
+        carPrice.setName("carPriceTextBox");
+        carAddPanel.add(carPrice);
+
+        carAddPanel.add(new Button("Submit", new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                carAddForm.submit();
+            }
+        }));
+
+        carAddForm.setEncoding(FormPanel.ENCODING_MULTIPART);
+        carAddForm.setMethod(FormPanel.METHOD_POST);
+        carAddForm.setWidget(carAddPanel);
+        carAddForm.addSubmitHandler(new FormPanel.SubmitHandler() {
+            @Override
+            public void onSubmit(FormPanel.SubmitEvent event) {
+                // code goes here
+                // validation goes here
+            }
+        });
+        carAddForm.addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler() {
+            @Override
+            public void onSubmitComplete(FormPanel.SubmitCompleteEvent event) {
+                // code goes here
+                Window.alert("Submitted!");
+            }
+        });
+
+        DecoratorPanel dp = new DecoratorPanel();
+        dp.add(carAddForm);
+        RootPanel.get("car-form").add(dp);
     }
 }
